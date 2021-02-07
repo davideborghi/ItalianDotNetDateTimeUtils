9fe1bb651a6f792cb1cbbfeb38061ac4904da926
Davide Borghi <davide.borghi@altran.it>
2021-02-07 17:50:41 +0100

    method documentation

--- DavideBorghi.ItalianDotNetDateTimeUtils/ItalianDotNetDateTimeUtils.cs
+++ DavideBorghi.ItalianDotNetDateTimeUtils/ItalianDotNetDateTimeUtils.cs
@@ -8,6 +8,9 @@ namespace DavideBorghi.ItalianDotNetDateTimeUtils
 {
 	public static class ItalianDotNetDateTimeUtils
 	{
+		/// <summary>
+		/// This variable is used to store national holidays
+		/// </summary>
 		public static List<string> giorniFestiviNazionaliList = new List<string>
 		{
 			"0101",
@@ -21,46 +24,83 @@ namespace DavideBorghi.ItalianDotNetDateTimeUtils
 			"2512",
 			"2612"
 		};
+		/// <summary>
+		/// Variable for local holidays such as "Santo Patrono"
+		/// Must be in format ddMM
+		/// </summary>
 		public static List<string> giorniFestiviLocaliList = new List<string>
 		{
 
 		};
 		private static List<DateTime> giorniFestivi;
-
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="dateAsString">the day in format ddMM</param>
+		/// <param name="year">the year you want the date</param>
+		/// <returns>Return day in DateTime</returns>
 		public static DateTime GetGiornoOfYearFromString(string dateAsString, int year)
         {
 			int giorno = int.Parse(dateAsString.Substring(0, 2));
 			int mese = int.Parse(dateAsString.Substring(2, 2));
 			return new DateTime(year, mese, giorno);
         }
-
+		/// <summary>
+		/// return the date of the Monday of the week of the given day
+		/// </summary>
+		/// <param name="dt"></param>
+		/// <param name="startOfWeek">Day you start of week, monday is default</param>
+		/// <returns>return the date of the Monday of the week of the given day</returns>
 		public static DateTime StartOfWeek(this DateTime dt, DayOfWeek startOfWeek = DayOfWeek.Monday)
 		{
 			int num = (7 + (dt.DayOfWeek - startOfWeek)) % 7;
 			return dt.AddDays(-1 * num).Date;
 		}
-
+		/// <summary>
+		/// return the date of the Sunday of the week of the given day
+		/// </summary>
+		/// <param name="dt"></param>
+		/// <param name="startOfWeek">Day you start of week, monday is default</param>
+		/// <returns>return the date of the Sunday of the week of the given day</returns>
 		public static DateTime EndOfWeek(this DateTime dt, DayOfWeek startOfWeek = DayOfWeek.Monday)
 		{
 			return StartOfWeek(dt).AddDays(6.0);
 		}
-
+		/// <summary>
+		/// Get the date of the last day of the month of the given date
+		/// </summary>
+		/// <param name="dateTime"></param>
+		/// <returns>date of the last day of the month of the given date</returns>
 		public static DateTime GetLastDayOfMonth(this DateTime dateTime)
 		{
 			return new DateTime(dateTime.Year, dateTime.Month, DateTime.DaysInMonth(dateTime.Year, dateTime.Month));
 		}
-
+		/// <summary>
+		/// Get the date of the first day of the month of the given date
+		/// </summary>
+		/// <param name="dateTime"></param>
+		/// <returns>date of the first day of the month of the given date</returns>
 		public static DateTime GetFirstDayOfMonth(this DateTime dateTime)
 		{
 			return new DateTime(dateTime.Year, dateTime.Month, 1);
 		}
 
-
+		/// <summary>
+		/// Get how many working days exists between two dates
+		/// </summary>
+		/// <param name="startDate">start date for calc</param>
+		/// <param name="endDate">end date for calc</param>
+		/// <returns></returns>
 		public static int HowManyWorkingDaysBetweenDates(DateTime startDate, DateTime endDate)
 		{
 			int num = 0;
 			DateTime dateTime = ((startDate < endDate) ? startDate : endDate);
 			DateTime dateTime2 = ((startDate < endDate) ? endDate : startDate);
+			int[] years = GetYearsBetweenDates(dateTime, dateTime2);
+			foreach(int year in years)
+            {
+				CalcolaGiorniFestiviPerAnno(year);
+            }
 			while (dateTime <= dateTime2)
 			{
 				if (!IsFestivo(dateTime))
@@ -71,18 +111,47 @@ namespace DavideBorghi.ItalianDotNetDateTimeUtils
 			}
 			return num;
 		}
-
+		/// <summary>
+		/// Get array of the year between two dates
+		/// </summary>
+		/// <param name="start"></param>
+		/// <param name="end"></param>
+		/// <returns></returns>
+		public static int[] GetYearsBetweenDates(DateTime start, DateTime end)
+        {
+			List<int> years = new List<int>();
+			int startYear = start.Year;
+			int endYear = end.Year;
+			while(startYear <= endYear)
+            {
+				years.Add(startYear++);
+            }
+			return years.ToArray();
+        }
+		/// <summary>
+		/// get if a date is holiday
+		/// </summary>
+		/// <param name="dateTime"></param>
+		/// <returns></returns>
 		public static bool IsFestivo(this DateTime dateTime)
 		{
-			giorniFestivi = CalcolaGiorniFestiviPerAnno(dateTime.Year);
+			if (giorniFestivi == null)
+            {
+				giorniFestivi = CalcolaGiorniFestiviPerAnno(dateTime.Year);
+			}
+			
 
 			return IsWeekend(dateTime) || giorniFestivi.Contains(dateTime) || GetEasterSunday(dateTime.Year).AddDays(1.0) == dateTime;
 		}
 
         private static List<DateTime> CalcolaGiorniFestiviPerAnno(int year)
         {
-			giorniFestivi = new List<DateTime>();
-            foreach(string giorno in giorniFestiviNazionaliList)
+			if (giorniFestivi == null)
+            {
+				giorniFestivi = new List<DateTime>();
+
+			}
+			foreach (string giorno in giorniFestiviNazionaliList)
             {
 				giorniFestivi.Add(GetGiornoOfYearFromString(giorno, year));
             }
@@ -93,11 +162,20 @@ namespace DavideBorghi.ItalianDotNetDateTimeUtils
 			}
 			return giorniFestivi;
 		}
-
+		/// <summary>
+		/// get if a day is weekend
+		/// </summary>
+		/// <param name="giorno"></param>
+		/// <returns></returns>
         public static bool IsWeekend(this DateTime giorno)
 		{
 			return giorno.DayOfWeek == DayOfWeek.Saturday || giorno.DayOfWeek == DayOfWeek.Sunday;
 		}
+		/// <summary>
+		/// given a year return the Easter sunday
+		/// </summary>
+		/// <param name="year"></param>
+		/// <returns></returns>
 
 		public static DateTime GetEasterSunday(int year)
 		{
