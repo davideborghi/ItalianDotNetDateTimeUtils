f395b1de7bc78b7697f14285a374e33b32149091
Massimo Serra <massimosrr@gmail.com>
2023-03-29 00:12:06 +0200

    Strengthen 'start and end week-related' unit tests

--- test/DavideBorghi.ItalianDotNetDateTimeUtils.Standard.Tests/UnitTests/DateTimeWeekExtensionsTests.cs
+++ test/DavideBorghi.ItalianDotNetDateTimeUtils.Standard.Tests/UnitTests/DateTimeWeekExtensionsTests.cs
@@ -15,6 +15,9 @@ namespace DavideBorghi.ItalianDotNetDateTimeUtils.Standard.Tests.UnitTests
         public void StartOfWeek_MustBeEqual(DateTime firstDateTime, DateTime secondDateTime)
         {
             Assert.Equal(firstDateTime.StartOfWeek(), secondDateTime.StartOfWeek());
+            Assert.Equal(firstDateTime.StartOfWeek(DayOfWeek.Monday), secondDateTime.StartOfWeek());
+            Assert.Equal(firstDateTime.StartOfWeek(DayOfWeek.Sunday), secondDateTime.StartOfWeek(DayOfWeek.Sunday));
+            Assert.Equal(firstDateTime.StartOfWeek(DayOfWeek.Monday), secondDateTime.StartOfWeek(DayOfWeek.Monday));
         }
 
         [Theory]
@@ -25,26 +28,18 @@ namespace DavideBorghi.ItalianDotNetDateTimeUtils.Standard.Tests.UnitTests
         public void StartOfWeek_MustBeDifferent(DateTime firstDateTime, DateTime secondDateTime)
         {
             Assert.NotEqual(firstDateTime.StartOfWeek(), secondDateTime.StartOfWeek());
-        }
-
-        [Theory]
-        [InlineData("2023/03/20", "2023/03/27")]
-        [InlineData("2022/03/20", "2023/03/20")]
-        [InlineData("2022/03/01", "2023/04/01")]
-        [InlineData("2023/03/01", "2022/04/01")]
-        public void StartOfWeek_WithSameWeekStartDay_MustBeDifferent(DateTime firstDateTime, DateTime secondDateTime)
-        {
             Assert.NotEqual(firstDateTime.StartOfWeek(DayOfWeek.Sunday), secondDateTime.StartOfWeek(DayOfWeek.Sunday));
+            Assert.NotEqual(firstDateTime.StartOfWeek(DayOfWeek.Sunday), secondDateTime.StartOfWeek(DayOfWeek.Tuesday));
         }
 
         [Theory]
-        [InlineData("2023/03/20", "2023/03/27")]
-        [InlineData("2022/03/20", "2023/03/20")]
-        [InlineData("2022/03/01", "2023/04/01")]
-        [InlineData("2023/03/01", "2022/04/01")]
-        public void StartOfWeek_WithDifferentWeekStartDay_MustBeDifferent(DateTime firstDateTime, DateTime secondDateTime)
+        [InlineData("2023/03/20", "2023/03/20")]
+        [InlineData("2023/03/20", "2023/03/21")]
+        public void StartOfWeek_WithDifferentStartOfWeekDay_MustBeDifferent(DateTime firstDateTime, DateTime secondDateTime)
         {
+            Assert.NotEqual(firstDateTime.StartOfWeek(DayOfWeek.Sunday), secondDateTime.StartOfWeek(DayOfWeek.Monday));
             Assert.NotEqual(firstDateTime.StartOfWeek(DayOfWeek.Sunday), secondDateTime.StartOfWeek(DayOfWeek.Tuesday));
+            Assert.NotEqual(firstDateTime.StartOfWeek(DayOfWeek.Tuesday), secondDateTime.StartOfWeek(DayOfWeek.Friday));
         }
 
         #endregion
@@ -57,6 +52,8 @@ namespace DavideBorghi.ItalianDotNetDateTimeUtils.Standard.Tests.UnitTests
         public void EndOfWeek_MustBeEqual(DateTime firstDateTime, DateTime secondDateTime)
         {
             Assert.Equal(firstDateTime.EndOfWeek(), secondDateTime.EndOfWeek());
+            Assert.Equal(firstDateTime.EndOfWeek(DayOfWeek.Monday), secondDateTime.EndOfWeek());
+            Assert.Equal(firstDateTime.EndOfWeek(DayOfWeek.Sunday), secondDateTime.EndOfWeek(DayOfWeek.Sunday));
         }
 
         [Theory]
@@ -67,6 +64,18 @@ namespace DavideBorghi.ItalianDotNetDateTimeUtils.Standard.Tests.UnitTests
         public void EndOfWeek_MustBeDifferent(DateTime firstDateTime, DateTime secondDateTime)
         {
             Assert.NotEqual(firstDateTime.EndOfWeek(), secondDateTime.EndOfWeek());
+            Assert.NotEqual(firstDateTime.EndOfWeek(DayOfWeek.Monday), secondDateTime.EndOfWeek());
+            Assert.NotEqual(firstDateTime.EndOfWeek(DayOfWeek.Sunday), secondDateTime.EndOfWeek(DayOfWeek.Sunday));
+            Assert.NotEqual(firstDateTime.EndOfWeek(DayOfWeek.Sunday), secondDateTime.EndOfWeek(DayOfWeek.Tuesday));
+        }
+
+        [Theory]
+        [InlineData("2023/03/20", "2023/03/20")]
+        [InlineData("2023/03/20", "2023/03/21")]
+        public void EndOfWeek_WithDifferentStartOfWeekDay_MustBeDifferent(DateTime firstDateTime, DateTime secondDateTime)
+        {
+            Assert.NotEqual(firstDateTime.EndOfWeek(DayOfWeek.Sunday), secondDateTime.EndOfWeek(DayOfWeek.Wednesday));
+            Assert.NotEqual(firstDateTime.EndOfWeek(DayOfWeek.Monday), secondDateTime.EndOfWeek(DayOfWeek.Tuesday));
         }
 
         #endregion
