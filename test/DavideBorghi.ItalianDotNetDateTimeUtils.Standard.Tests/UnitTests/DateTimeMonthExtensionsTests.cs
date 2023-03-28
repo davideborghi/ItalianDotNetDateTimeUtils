0cadaa4c288d51f39195021015abe0817a2cc3bc
Massimo Serra <massimosrr@gmail.com>
2023-03-29 00:46:41 +0200

    Add first 'month-based' unit tests

---
+++ test/DavideBorghi.ItalianDotNetDateTimeUtils.Standard.Tests/UnitTests/DateTimeMonthExtensionsTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DavideBorghi.ItalianDotNetDateTimeUtils.Standard.Tests.UnitTests
+{
+    public class DateTimeMonthExtensionsTests
+    {
+        #region MonthOfYear
+
+        [Fact]
+        public void MonthOfYear_MustBeJanuary()
+        {
+            Assert.Equal(DateTimeEnums.MonthOfYear.January, new DateTime(2023, 1, 1).MonthOfYear());
+        }
+
+        [Theory]
+        [InlineData("2023/07/16")]
+        public void MonthOfYear_MustBeAsExpected(DateTime datetime, DateTimeEnums.MonthOfYear expectedMonthOfYear = DateTimeEnums.MonthOfYear.July)
+        {
+            Assert.Equal(datetime.MonthOfYear(), expectedMonthOfYear);
+        }
+
+        [Theory]
+        [InlineData("2022/01/01", "2022/01/01")]
+        [InlineData("2023/07/16", "2023/07/16")]
+        [InlineData("2022/07/16", "2023/07/01")]
+        [InlineData("2022/07/16", "2022/07/01")]
+        public void MonthOfYear_MustBeEqual(DateTime firstDateTime, DateTime secondDateTime)
+        {
+            Assert.Equal(firstDateTime.MonthOfYear(), secondDateTime.MonthOfYear());
+        }
+
+        [Theory]
+        [InlineData("2023/06/16", "2023/07/01")]
+        public void MonthOfYear_MustBeDifferent(DateTime firstDateTime, DateTime secondDateTime)
+        {
+            Assert.NotEqual(firstDateTime.MonthOfYear(), secondDateTime.MonthOfYear());
+        }
+
+        #endregion
+
+        #region FirstDayOfMonth
+
+        #endregion
+
+        #region LastDayOfMonth
+
+        #endregion
+    }
+}
